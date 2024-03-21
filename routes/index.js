38d425d0059b2ee9b6c2d5d188531d740ac4bfd5
Mustafa Ali <mustafaalijah@gmail.com>
2024-03-21 13:10:08 +0500

    commit2

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,7 @@ const router = express.Router();
 const authRouter = require("./auth");
 router.use("/auth", authRouter);
 
+const purchaseRouter = require("./purchase");
+router.use("/purchase", purchaseRouter)
+
 module.exports = router;
